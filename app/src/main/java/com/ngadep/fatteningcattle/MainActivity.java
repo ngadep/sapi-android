5ac2b582bb715ad2345d8a849f266a923dead3a9
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-05 08:50:43 +0700

    update main activity create package.

--- app/src/main/java/com/ngadep/fatteningcattle/MainActivity.java
+++ app/src/main/java/com/ngadep/fatteningcattle/MainActivity.java
@@ -3,22 +3,35 @@ package com.ngadep.fatteningcattle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.ngadep.fatteningcattle.models.Package;
+import com.ngadep.fatteningcattle.models.User;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     // [START declare_database_ref]
     private DatabaseReference mDatabase;
     // [END declare_database_ref]
 
     TextView mText;
-    Button mButton;
+    Button mBtnSignOut, mBtnRun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +39,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mText = (TextView) findViewById(R.id.tx_hello);
-        mButton = (Button) findViewById(R.id.btn_sign_out);
-        mButton.setOnClickListener(this);
+
+        mBtnSignOut = (Button) findViewById(R.id.btn_sign_out);
+        mBtnSignOut.setOnClickListener(this);
+
+        mBtnRun = (Button) findViewById(R.id.btn_run);
+        mBtnRun.setOnClickListener(this);
 
         mText.setText("Hallo " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        // [START create_database_reference]
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        // [END create_database_reference]
     }
 
     public void signOut() {
@@ -43,6 +63,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int i = v.getId();
         if (i == R.id.btn_sign_out) {
             signOut();
+        } else if (i== R.id.btn_run) {
+            createPackage();
         }
+    }
+
+    private void createPackage() {
+        // [START single_value_read]
+        final String userId = getUid();
+        mDatabase.child("users").child(userId).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        User user = dataSnapshot.getValue(User.class);
+
+                        // [START_EXCLUDE]
+                        if (user == null) {
+                            // User is null, error out
+                            Log.e(TAG, "User " + userId + " is unexpectedly null");
+                            Toast.makeText(MainActivity.this,
+                                    "Error: could not fetch user.",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Write new post
+                            writeNewPackage(userId);
+                        }
+
+                        // Finish this Activity, back to the stream
+                        // finish();
+                        // [END_EXCLUDE]
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w(TAG, "getUser:onCancelled", databaseError.toException());
+                        // [START_EXCLUDE]
+                        // setEditingEnabled(true);
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END single_value_read]
+    }
+
+    private void writeNewPackage(String userId) {
+        // Create new post at /user-posts/$userid/$postid and at
+        // /posts/$postid simultaneously
+        String key = mDatabase.child("packages").push().getKey();
+        Package aPackage = new Package("paket S001", "sumur", 5, true);
+        Map<String, Object> postValues = aPackage.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/packages/" + key, postValues);
+        childUpdates.put("/user-packages/" + userId + "/" + key, postValues);
+
+        mDatabase.updateChildren(childUpdates);
+
     }
 }
