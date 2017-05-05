09254f37cfd9826023ba89df09c13e04cbb4466c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-05 08:06:03 +0700

    update main activity

--- app/src/main/java/com/ngadep/fatteningcattle/MainActivity.java
+++ app/src/main/java/com/ngadep/fatteningcattle/MainActivity.java
@@ -1,13 +1,48 @@
 package com.ngadep.fatteningcattle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // [START declare_database_ref]
+    private DatabaseReference mDatabase;
+    // [END declare_database_ref]
+
+    TextView mText;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mText = (TextView) findViewById(R.id.tx_hello);
+        mButton = (Button) findViewById(R.id.btn_sign_out);
+        mButton.setOnClickListener(this);
+
+        mText.setText("Hallo " + FirebaseAuth.getInstance().getCurrentUser().getEmail());
+    }
+
+    public void signOut() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(this, SignInActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.btn_sign_out) {
+            signOut();
+        }
     }
 }
