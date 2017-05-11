3ae3f997b2acdc7cbb6d897176286b9a9c13b204
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-11 21:29:34 +0700

    update login presenter

--- app/src/main/java/com/ngadep/fatteningcattle/presenter/LoginPresenter.java
+++ app/src/main/java/com/ngadep/fatteningcattle/presenter/LoginPresenter.java
@@ -2,7 +2,7 @@ package com.ngadep.fatteningcattle.presenter;
 
 import com.ngadep.fatteningcattle.contracts.LoginContract;
 
-public class LoginPresenter implements LoginContract.Presenter {
+public class LoginPresenter {
 
     private LoginContract.View mLogInView;
     private LoginContract.Repository mLogInRepository;
@@ -12,13 +12,31 @@ public class LoginPresenter implements LoginContract.Presenter {
         mLogInRepository = logInRepository;
     }
 
-    @Override
-    public boolean isLogin() {
+    private boolean isLoggedIn() {
         return mLogInRepository.isLogin();
     }
 
-    @Override
-    public void onLogInFailed(int code) {
-        mLogInView.showLoginFailed(code);
+    public void tryToLogIn() {
+        mLogInView.tryLogIn();
     }
+
+    public void start() {
+        if (this.isLoggedIn()) {
+            mLogInView.startMainActivity();
+        } else {
+            mLogInView.tryLogIn();
+        }
+    }
+
+    public void onLoginResult(int resultCode) {
+        int LOGIN_OK = -1;
+        if (resultCode == LOGIN_OK) {
+            mLogInView.showTextAndButton(false);
+            mLogInView.startMainActivity();
+        } else {
+            mLogInView.showTextAndButton(true);
+            mLogInView.showLoginFailed(resultCode);
+        }
+    }
+
 }
