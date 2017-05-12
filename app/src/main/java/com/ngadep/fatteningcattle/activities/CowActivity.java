78a1fddf1f0d750a7d7b48754debb0a1036f9889
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-12 20:46:07 +0700

    CowActivity

---
+++ app/src/main/java/com/ngadep/fatteningcattle/activities/CowActivity.java
@@ -0,0 +1,15 @@
+package com.ngadep.fatteningcattle.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ngadep.fatteningcattle.R;
+
+public class CowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cow);
+    }
+}
