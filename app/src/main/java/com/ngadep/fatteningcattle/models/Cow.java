31efd38c6b12c9623de1415aff1d4afce7af901b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-12 20:09:02 +0700

    tanggal dan harga untuk sapi

--- app/src/main/java/com/ngadep/fatteningcattle/models/Cow.java
+++ app/src/main/java/com/ngadep/fatteningcattle/models/Cow.java
@@ -1,18 +1,24 @@
 package com.ngadep.fatteningcattle.models;
 
+import java.util.Date;
+
 public class Cow {
     private String earTag;
     private String sex;
     private int weight;
+    private Long price;
+    private Date date;
 
     public Cow() {
         // Default constructor required for calls to DataSnapshot.getValue(Cow.class)
     }
 
-    public Cow(String earTag, String sex, int weight) {
+    public Cow(String earTag, String sex, int weight, Long price, Date date) {
         this.earTag = earTag;
         this.sex = sex;
         this.weight = weight;
+        this.price = price;
+        this.date = date;
     }
 
     public String getEarTag() {
@@ -37,5 +43,21 @@ public class Cow {
 
     public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
