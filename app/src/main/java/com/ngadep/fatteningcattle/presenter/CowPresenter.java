ac86d122827db72afb71926fd41625eddbdbc8bf
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-12 23:17:13 +0700

    cow presenter

---
+++ app/src/main/java/com/ngadep/fatteningcattle/presenter/CowPresenter.java
@@ -0,0 +1,23 @@
+package com.ngadep.fatteningcattle.presenter;
+
+import com.ngadep.fatteningcattle.contracts.CowContract;
+
+public class CowPresenter {
+    private final CowContract.View mView;
+    private final CowContract.Repository mRepository;
+    private final String mCowId;
+
+    public CowPresenter(CowContract.View view, CowContract.Repository repository, String packageId) {
+        mView = view;
+        mRepository = repository;
+        mCowId = packageId;
+    }
+
+    public void getPackageCows() {
+        mView.getAllPackageCow(mRepository.getPackageCowFromId(mCowId));
+    }
+
+    public void startCowDetailActivity(String cowId) {
+        /* TODO: Add Cow Detail Activity */
+    }
+}
