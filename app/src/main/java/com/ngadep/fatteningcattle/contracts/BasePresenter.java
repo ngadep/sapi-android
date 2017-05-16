235a96a90078ac4e566855ffa39bea40e3ffae97
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-16 21:22:47 +0700

    BaseView and BasePresenter.

---
+++ app/src/main/java/com/ngadep/fatteningcattle/contracts/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.ngadep.fatteningcattle.contracts;
+
+public interface BasePresenter {
+
+    void start();
+
+}
---
+++ app/src/main/java/com/ngadep/fatteningcattle/contracts/BaseView.java
@@ -0,0 +1,7 @@
+package com.ngadep.fatteningcattle.contracts;
+
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+
+}
