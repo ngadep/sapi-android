aef9dcaaafd8a4ed6964f8a567fc37c0430b1377
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-18 15:32:13 +0700

    MainActivity extends BaseMainActivity

--- app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
@@ -1,104 +1,20 @@
 package com.ngadep.fatteningcattle.main;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.google.firebase.auth.FirebaseAuth;
 import com.ngadep.fatteningcattle.R;
 import com.ngadep.fatteningcattle.news.NewsFragment;
 import com.ngadep.fatteningcattle.packages.PackageFragment;
-import com.ngadep.fatteningcattle.login.LogInActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String TAG = "MainActivity";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        SectionsPagerAdapter mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
-    }
-
+public class MainActivity extends BaseMainActivity {
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public List<MainFragment> getFragments() {
+        List<MainFragment> result = new ArrayList<>();
+        result.add(new MainFragment(R.string.heading_main_package, new PackageFragment()));
+        result.add(new MainFragment(R.string.heading_main_news, new NewsFragment()));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_logout) {
-            Log.i(TAG, "User Logged Out");
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(this, LogInActivity.class));
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    private class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        private final Fragment[] mFragments = new Fragment[] {
-                new PackageFragment(),
-                new NewsFragment()
-        };
-        private final String[] mFragmentNames = new String[] {
-                getString(R.string.heading_main_package),
-                getString(R.string.heading_main_news)
-        };
-
-        SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            return mFragments[position];
-        }
-
-        @Override
-        public int getCount() {
-            // Show total fragments.
-            return mFragments.length;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return mFragmentNames[position];
-        }
+        return result;
     }
 }
---
+++ app/src/main/java/com/ngadep/fatteningcattle/main/BaseMainActivity.java
@@ -0,0 +1,106 @@
+package com.ngadep.fatteningcattle.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.ngadep.fatteningcattle.R;
+import com.ngadep.fatteningcattle.login.LogInActivity;
+
+import java.util.List;
+
+public abstract class BaseMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        SectionsPagerAdapter mSectionsPagerAdapter = getSectionPagerAdapter();
+
+        // Set up the ViewPager with the sections adapter.
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, LogInActivity.class));
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MainFragment {
+        int resourceText;
+        Fragment fragment;
+
+        MainFragment(int resourceText, Fragment fragment) {
+            this.resourceText = resourceText;
+            this.fragment = fragment;
+        }
+    }
+
+    public abstract List<MainFragment> getFragments();
+
+    private SectionsPagerAdapter getSectionPagerAdapter() {
+        return new SectionsPagerAdapter(getSupportFragmentManager(), getFragments());
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        List<MainFragment> mFragments;
+
+        SectionsPagerAdapter(FragmentManager fm, List<MainFragment> fragment) {
+            super(fm);
+            mFragments = fragment;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            return mFragments.get(position).fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // Show total fragments.
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getString(mFragments.get(position).resourceText);
+        }
+    }
+}
