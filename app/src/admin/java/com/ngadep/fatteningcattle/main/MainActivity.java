85e5a74180b65986949dc5e79385f38bad7a8d76
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-21 20:33:08 +0700

    User Tab on MainActivity

--- app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
@@ -2,7 +2,7 @@ package com.ngadep.fatteningcattle.main;
 
 import com.ngadep.fatteningcattle.R;
 import com.ngadep.fatteningcattle.news.NewsFragment;
-import com.ngadep.fatteningcattle.packages.PackageFragment;
+import com.ngadep.fatteningcattle.users.UserFragment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +12,7 @@ public class MainActivity extends BaseMainActivity {
     @Override
     public List<MainFragment> getFragments() {
         List<MainFragment> result = new ArrayList<>();
-        result.add(new MainFragment(R.string.heading_main_package, new PackageFragment()));
+        result.add(new MainFragment(R.string.heading_main_user, UserFragment.newInstance()));
         result.add(new MainFragment(R.string.heading_main_news, new NewsFragment()));
 
         return result;
