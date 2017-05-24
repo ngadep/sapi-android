33dc9b5126c40f881db22473edc9cfa3c170a4bb
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-24 22:52:33 +0700

    add package activity

---
+++ app/src/main/java/com/ngadep/fatteningcattle/packages/PackageActivity.java
@@ -0,0 +1,15 @@
+package com.ngadep.fatteningcattle.packages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ngadep.fatteningcattle.R;
+
+public class PackageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package);
+    }
+}
