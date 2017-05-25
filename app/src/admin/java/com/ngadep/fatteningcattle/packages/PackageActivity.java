a29e309ab9ed8f1eeddfd7a42a173dba55220489
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-25 21:25:00 +0700

    tambahkan package fragment ke activity

--- app/src/admin/java/com/ngadep/fatteningcattle/packages/PackageActivity.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/packages/PackageActivity.java
@@ -4,12 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ngadep.fatteningcattle.R;
+import com.ngadep.fatteningcattle.utils.ActivityUtils;
 
 public class PackageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USER_ID = "extra user id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_package);
+
+        // Get the requested user id
+        String taskId = getIntent().getStringExtra(EXTRA_USER_ID);
+
+        PackageFragment packageFragment = (PackageFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.content_frame);
+
+        if (packageFragment == null) {
+            // Create the fragment
+            packageFragment = new PackageFragment();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), packageFragment, R.id.content_frame);
+        }
+
     }
 }
