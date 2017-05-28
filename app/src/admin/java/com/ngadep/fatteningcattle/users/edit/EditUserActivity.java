555a9282cd5ff6ccee5f43029da2769fe663f6d4
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-05-28 21:32:16 +0700

    add EditUserActivity

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/users/edit/EditUserActivity.java
@@ -0,0 +1,15 @@
+package com.ngadep.fatteningcattle.users.edit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ngadep.fatteningcattle.R;
+
+public class EditUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+    }
+}
