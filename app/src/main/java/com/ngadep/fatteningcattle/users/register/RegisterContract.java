1afccbea3c2a1779c30ade999fa29101139fa660
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-01 05:08:32 +0700

    update RegisterContract

--- app/src/main/java/com/ngadep/fatteningcattle/users/register/RegisterContract.java
+++ app/src/main/java/com/ngadep/fatteningcattle/users/register/RegisterContract.java
@@ -5,7 +5,7 @@ import com.ngadep.fatteningcattle.BasePresenter;
 import com.ngadep.fatteningcattle.BaseView;
 import com.ngadep.fatteningcattle.models.User;
 
-public interface RegisterContract {
+interface RegisterContract {
     interface Presenter extends BasePresenter{
 
         void saveUser(User user, String password);
@@ -13,6 +13,8 @@ public interface RegisterContract {
 
     interface View extends BaseView<Presenter> {
 
-        void showUser(User user);
+        void loginSuccess();
+
+        void showLoginFailed();
     }
 }
