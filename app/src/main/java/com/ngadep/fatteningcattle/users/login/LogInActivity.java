0c9f46ca191d46f2b1cc0744c4d25469f06819f4
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-08 20:21:38 +0700

    ubah LogInActivity menjadi menggunakan fragment

--- app/src/main/java/com/ngadep/fatteningcattle/users/login/LogInActivity.java
+++ app/src/main/java/com/ngadep/fatteningcattle/users/login/LogInActivity.java
@@ -1,142 +1,30 @@
 package com.ngadep.fatteningcattle.users.login;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.ngadep.fatteningcattle.R;
-import com.ngadep.fatteningcattle.main.MainActivity;
 import com.ngadep.fatteningcattle.users.UserRepository;
-import com.ngadep.fatteningcattle.users.register.RegisterActivity;
+import com.ngadep.fatteningcattle.utils.ActivityUtils;
 
-public class LogInActivity extends AppCompatActivity implements LoginContract.View {
-
-    private static final String TAG = "SignInActivity";
-    private static final int REQUEST_REGISTER = 1;
-
-    private LoginContract.Presenter mPresenter;
-    private EditText mEmailField;
-    private EditText mPasswordField;
-    private Button mSignInButton;
-    private ProgressDialog mProgressDialog;
+public class LogInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_act);
 
-        UserRepository mRepository = UserRepository.getInstance();
-        mPresenter = new LoginPresenter(this, mRepository);
+        LoginFragment loginFragment = (LoginFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.content_frame);
 
-        // Views
-        mEmailField = (EditText) findViewById(R.id.field_email);
-        mPasswordField = (EditText) findViewById(R.id.field_password);
-        mSignInButton = (Button) findViewById(R.id.btn_sign_in);
-        Button mRegister = (Button) findViewById(R.id.btn_login);
-
-        // Click listeners
-        mSignInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tryToLogIn();
-            }
-        });
-
-        mRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
-                startActivityForResult(intent, REQUEST_REGISTER);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (REQUEST_REGISTER== requestCode && Activity.RESULT_OK == resultCode) {
-            startMainActivity();
+        if (loginFragment == null) {
+            // Create the fragment
+            loginFragment = LoginFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), loginFragment, R.id.content_frame);
         }
 
+        new LoginPresenter(loginFragment, UserRepository.getInstance());
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "on start");
-        mPresenter.start();
-    }
-
-    private void tryToLogIn() {
-        Log.d(TAG, "signIn");
-        if (!validateForm()) {
-            return;
-        }
-
-        String email = mEmailField.getText().toString();
-        String password = mPasswordField.getText().toString();
-        mPresenter.tryToLogIn(email, password);
-    }
-
-    private boolean validateForm() {
-        boolean result = true;
-        if (TextUtils.isEmpty(mEmailField.getText().toString())) {
-            mEmailField.setError("Required");
-            result = false;
-        } else {
-            mEmailField.setError(null);
-        }
-
-        if (TextUtils.isEmpty(mPasswordField.getText().toString())) {
-            mPasswordField.setError("Required");
-            result = false;
-        } else {
-            mPasswordField.setError(null);
-        }
-
-        return result;
-    }
-
-    @Override
-    public void startMainActivity() {
-        // Go to MainActivity
-        startActivity(new Intent(LogInActivity.this, MainActivity.class));
-        finish();
-    }
-
-    @Override
-    public void showErrorText() {
-        Snackbar.make(mSignInButton, "Login Failed", Snackbar.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void showProgressDialog() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setCancelable(false);
-            mProgressDialog.setMessage("Loading...");
-        }
-
-        mProgressDialog.show();
-    }
-
-    @Override
-    public void hideProgressDialog() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.dismiss();
-        }
-    }
-
-    @Override
-    public void setPresenter(LoginContract.Presenter presenter) {
-        mPresenter = presenter;
-    }
 }
