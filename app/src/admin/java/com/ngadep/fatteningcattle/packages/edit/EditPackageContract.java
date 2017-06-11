88eb845e40d90ca6cd342c800509056420dba95d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-11 21:53:48 +0700

    EditPackageContract

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/packages/edit/EditPackageContract.java
@@ -0,0 +1,30 @@
+package com.ngadep.fatteningcattle.packages.edit;
+
+import com.ngadep.fatteningcattle.BasePresenter;
+import com.ngadep.fatteningcattle.BaseView;
+
+public interface EditPackageContract {
+    interface Presenter extends BasePresenter {
+
+        boolean isNewPackage();
+
+        void populatePackage();
+
+        void savePackage(String name, String location, int type, boolean active);
+    }
+
+    interface View extends BaseView<Presenter>{
+
+        boolean isActive();
+
+        void setName(String name);
+
+        void setLocation(String location);
+
+        void setType(int type);
+
+        void setActive(boolean active);
+
+        void showPackageList();
+    }
+}
