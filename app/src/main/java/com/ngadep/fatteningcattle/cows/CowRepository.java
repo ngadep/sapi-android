2f944227b3eae1c74a9008c8c81d4f4ffd717d85
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-12 01:16:11 +0700

    CowRepository getPricePerKg

--- app/src/main/java/com/ngadep/fatteningcattle/cows/CowRepository.java
+++ app/src/main/java/com/ngadep/fatteningcattle/cows/CowRepository.java
@@ -1,10 +1,13 @@
 package com.ngadep.fatteningcattle.cows;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ngadep.fatteningcattle.BaseRepository;
 
-public class CowRepository extends BaseRepository implements CowContract.Repository {
+public class CowRepository extends BaseRepository {
     private static CowRepository INSTANCE = null;
     private final DatabaseReference mRef;
 
@@ -21,8 +24,26 @@ public class CowRepository extends BaseRepository implements CowContract.Reposit
         return INSTANCE;
     }
 
-    @Override
     public Query getPackageCowFromId(String packageId) {
         return mRef.child(packageId);
+    }
+
+    public void getPricePerKg(final PriceListener callback) {
+        DatabaseReference priceRef = getRef().child("settings").child("price");
+        priceRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onPriceChange((Long) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    interface PriceListener {
+        void onPriceChange(Long price);
     }
 }
