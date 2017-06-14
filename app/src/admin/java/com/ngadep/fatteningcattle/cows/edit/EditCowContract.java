d88dc07577cd4f295f9e10cff7986b015ae98857
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-14 23:43:27 +0700

    EditCowContract

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowContract.java
@@ -0,0 +1,14 @@
+package com.ngadep.fatteningcattle.cows.edit;
+
+import com.ngadep.fatteningcattle.BasePresenter;
+import com.ngadep.fatteningcattle.BaseView;
+
+public interface EditCowContract {
+    interface Presenter extends BasePresenter {
+
+    }
+
+    interface View extends BaseView<Presenter> {
+
+    }
+}
