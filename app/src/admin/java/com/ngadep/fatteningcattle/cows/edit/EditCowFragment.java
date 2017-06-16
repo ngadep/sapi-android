6954a8c698e100da25849b456bbaae2c7f7dac70
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-16 15:33:00 +0700

    instance for EditCowFragment.

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
@@ -3,10 +3,17 @@ package com.ngadep.fatteningcattle.cows.edit;
 import android.support.v4.app.Fragment;
 
 public class EditCowFragment extends Fragment implements EditCowContract.View {
+    public static final String ARGUMENT_EDIT_COW_ID = "EDIT_COW_ID";
+    public static final String ARGUMENT_EDIT_COW_MODEL = "EDIT_COW_MODEL";
+
     private EditCowContract.Presenter mPresenter;
 
     @Override
     public void setPresenter(EditCowContract.Presenter presenter) {
         mPresenter = presenter;
+    }
+
+    public static EditCowFragment newInstance() {
+        return new EditCowFragment();
     }
 }
