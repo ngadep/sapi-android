466918696cffa88489f3ada991c2aff8f0653c0f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-16 15:33:46 +0700

    constructor for EditCowPresenter.

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowPresenter.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowPresenter.java
@@ -1,6 +1,22 @@
 package com.ngadep.fatteningcattle.cows.edit;
 
+import com.ngadep.fatteningcattle.cows.CowRepository;
+import com.ngadep.fatteningcattle.models.Cow;
+
 public class EditCowPresenter implements EditCowContract.Presenter {
+    private final String mCowId;
+    private final Cow mCowModel;
+    private final CowRepository mRepository;
+    private final EditCowContract.View mView;
+
+    public EditCowPresenter(String cowId, Cow cowModel, CowRepository repository, EditCowContract.View view) {
+        mCowId = cowId;
+        mCowModel = cowModel;
+        mRepository = repository;
+        mView = view;
+        mView.setPresenter(this);
+    }
+
     @Override
     public void start() {
 
