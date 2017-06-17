3ace633febb0b000c7394c3ff31c8f6af11bca63
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-17 21:45:27 +0700

    extends CowFragment from BaseCowFragment

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/CowFragment.java
@@ -0,0 +1,11 @@
+package com.ngadep.fatteningcattle.cows;
+
+public class CowFragment extends BaseCowFragment {
+
+    public CowFragment() {
+    }
+
+    public static CowFragment newInstance() {
+        return new CowFragment();
+    }
+}
