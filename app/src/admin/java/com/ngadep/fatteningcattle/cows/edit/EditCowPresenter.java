39e2fd45a02e39f364c1d65932225d4aa4657d49
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-19 21:57:52 +0700

    update EditCowPresenter
    ubah constructor dengan menyertakan packageId.
    tampilkan data jika data bukan baru.
    simpan cow ke database.

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowPresenter.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowPresenter.java
@@ -1,15 +1,21 @@
 package com.ngadep.fatteningcattle.cows.edit;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
 import com.ngadep.fatteningcattle.cows.CowRepository;
 import com.ngadep.fatteningcattle.models.Cow;
 
 public class EditCowPresenter implements EditCowContract.Presenter {
+    private final String mPackageId;
     private final String mCowId;
     private final Cow mCowModel;
     private final CowRepository mRepository;
     private final EditCowContract.View mView;
 
-    public EditCowPresenter(String cowId, Cow cowModel, CowRepository repository, EditCowContract.View view) {
+    public EditCowPresenter(@NonNull String packageId, @Nullable String cowId, @Nullable Cow cowModel,
+                            @NonNull CowRepository repository, @NonNull EditCowContract.View view) {
+        mPackageId = packageId;
         mCowId = cowId;
         mCowModel = cowModel;
         mRepository = repository;
@@ -19,6 +25,33 @@ public class EditCowPresenter implements EditCowContract.Presenter {
 
     @Override
     public void start() {
+        if (!isNewCow()) {
+            populateCow();
+        }
+    }
 
+    private void populateCow() {
+        if (mView.isActive()) {
+            mView.setEarTag(mCowModel.getEar_tag());
+            mView.setSex(mCowModel.getSex());
+            mView.setWeight(mCowModel.getWeight());
+            mView.setDate(mCowModel.getLongDate());
+        }
+    }
+
+    @Override
+    public void saveCow(String earTag, String sex, int weight, long date) {
+        Cow cow = new Cow(mPackageId, earTag, sex, weight, date);
+        if (isNewCow()) {
+            mRepository.saveCow(cow);
+        } else {
+            mRepository.saveCow(mCowId, cow);
+        }
+        mView.showCowList();
+
+    }
+
+    public boolean isNewCow() {
+        return mCowId != null;
     }
 }
