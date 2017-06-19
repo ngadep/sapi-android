bfcb212ca04fb632196c9f0263bb6e68139a41c9
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-19 20:35:10 +0700

    view template.

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
@@ -1,6 +1,15 @@
 package com.ngadep.fatteningcattle.cows.edit;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.ngadep.fatteningcattle.R;
 
 public class EditCowFragment extends Fragment implements EditCowContract.View {
     public static final String ARGUMENT_EDIT_COW_ID = "EDIT_COW_ID";
@@ -8,12 +17,35 @@ public class EditCowFragment extends Fragment implements EditCowContract.View {
 
     private EditCowContract.Presenter mPresenter;
 
+    EditText mEarTag;
+    Spinner mSex;
+    EditText mWeight;
+    EditText mDate;
+
+    public static EditCowFragment newInstance() {
+        return new EditCowFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.edit_cow_frag, container, false);
+
+        mEarTag = (EditText) view.findViewById(R.id.ed_ear_tag);
+        mSex = (Spinner) view.findViewById(R.id.sp_sex);
+        ArrayAdapter<CharSequence> sexAdapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.sex_array, android.R.layout.simple_spinner_item);
+        sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSex.setAdapter(sexAdapter);
+        mWeight = (EditText) view.findViewById(R.id.ed_weight);
+        mDate = (EditText) view.findViewById(R.id.date);
+
+        return view;
+    }
+
     @Override
     public void setPresenter(EditCowContract.Presenter presenter) {
         mPresenter = presenter;
     }
 
-    public static EditCowFragment newInstance() {
-        return new EditCowFragment();
-    }
 }
