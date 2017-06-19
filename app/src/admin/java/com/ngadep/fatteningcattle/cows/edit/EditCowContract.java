f6099500eeb68ed6161b3be2040adac9099d8805
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-19 21:53:39 +0700

    update EditCowContract

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowContract.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowContract.java
@@ -11,5 +11,16 @@ public interface EditCowContract {
 
     interface View extends BaseView<Presenter> {
 
+        boolean isActive();
+
+        void setEarTag(String earTag);
+
+        void setSex(String sex);
+
+        void setWeight(int weight);
+
+        void setDate(Long date);
+
+        void showCowList();
     }
 }
