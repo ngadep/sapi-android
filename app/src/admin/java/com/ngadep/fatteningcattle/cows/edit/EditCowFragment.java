6e071875baa0d291817c1520713108d09c04975e
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-19 21:32:49 +0700

    simpan cow

--- app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/cows/edit/EditCowFragment.java
@@ -1,15 +1,23 @@
 package com.ngadep.fatteningcattle.cows.edit;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.ngadep.fatteningcattle.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class EditCowFragment extends Fragment implements EditCowContract.View {
     public static final String ARGUMENT_EDIT_COW_ID = "EDIT_COW_ID";
@@ -21,6 +29,7 @@ public class EditCowFragment extends Fragment implements EditCowContract.View {
     Spinner mSex;
     EditText mWeight;
     EditText mDate;
+    final Calendar mCalendar = Calendar.getInstance();
 
     public static EditCowFragment newInstance() {
         return new EditCowFragment();
@@ -38,9 +47,82 @@ public class EditCowFragment extends Fragment implements EditCowContract.View {
         sexAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSex.setAdapter(sexAdapter);
         mWeight = (EditText) view.findViewById(R.id.ed_weight);
-        mDate = (EditText) view.findViewById(R.id.date);
+        mDate = (EditText) view.findViewById(R.id.ed_date);
+
+        mDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mCalendar.set(year, month, dayOfMonth);
+                        mDate.setText(SimpleDateFormat.getDateInstance().format(mCalendar.getTime()));
+                    }
+                },
+                        mCalendar.get(Calendar.YEAR), mCalendar.get(Calendar.MONTH),
+                        mCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        FloatingActionButton fab =
+                (FloatingActionButton) getActivity().findViewById(R.id.fab_edit_cow);
+        fab.setImageResource(R.drawable.ic_done);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateForm()) {
+                    return;
+                }
+
+                String lSex;
+                if (mSex.getSelectedItemPosition() == 0 ) {
+                    lSex = "male";
+                } else {
+                    lSex = "female";
+                }
+
+                mPresenter.saveCow(
+                        mEarTag.getText().toString(),
+                        lSex,
+                        Integer.parseInt(mWeight.getText().toString()),
+                        mCalendar.getTime().getTime()
+                );
+            }
+        });
+
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(mEarTag.getText().toString())) {
+            mEarTag.setError("Required");
+            result = false;
+        } else {
+            mEarTag.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mWeight.getText().toString())) {
+            mWeight.setError("Required");
+            result = false;
+        } else {
+            mWeight.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mDate.getText().toString())) {
+            mDate.setError("Required");
+            result = false;
+        } else {
+            mDate.setError(null);
+        }
+
+        return result;
     }
 
     @Override
