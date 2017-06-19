578974e15c65ab6797bfad7c7304d9186d6c355b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-20 00:04:14 +0700

    simpan cow progress ketika membuat sapi baru.

--- app/src/main/java/com/ngadep/fatteningcattle/cows/CowRepository.java
+++ app/src/main/java/com/ngadep/fatteningcattle/cows/CowRepository.java
@@ -7,6 +7,7 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.ngadep.fatteningcattle.BaseRepository;
 import com.ngadep.fatteningcattle.models.Cow;
+import com.ngadep.fatteningcattle.models.Progress;
 
 public class CowRepository extends BaseRepository {
     private static CowRepository INSTANCE = null;
@@ -56,12 +57,30 @@ public class CowRepository extends BaseRepository {
         this.saveCow(null, cow);
     }
 
-    public void saveCow(String mCowId, Cow cow) {
-        if (mCowId == null) {
-            mCowId = mRef.push().getKey();
+    public void saveCow(String cowId, Cow cow) {
+        DatabaseReference localRef = mRef.child(cow.getPackage_id());
+
+        if (cowId == null) {
+            cowId = localRef.push().getKey();
+            Progress lProgress = new Progress(cowId, cow.getDate(), cow.getWeight());
+            this.saveCowProgress(lProgress);
         }
 
-        mRef.child(cow.getPackage_id()).child(mCowId).setValue(cow);
+        localRef.child(cowId).setValue(cow);
+    }
+
+    public void saveCowProgress(Progress progress) {
+        this.saveCowProgress(null, progress);
+    }
+
+    public void saveCowProgress(String progressId, Progress progress) {
+        DatabaseReference localRef = getRef().child("cow-progresses").child(progress.getCow_id());
+
+        if (progressId == null) {
+            progressId = localRef.push().getKey();
+        }
+
+        localRef.child(progressId).setValue(progress);
     }
 
     interface PriceListener {
