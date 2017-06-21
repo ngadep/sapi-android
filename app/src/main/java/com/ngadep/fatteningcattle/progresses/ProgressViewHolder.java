e0d9c68c9e58f816c5d622f154174d54fad73037
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-21 20:03:04 +0700

    ProgressViewHolder

---
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressViewHolder.java
@@ -0,0 +1,11 @@
+package com.ngadep.fatteningcattle.progresses;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ProgressViewHolder extends RecyclerView.ViewHolder {
+
+    public ProgressViewHolder(View itemView) {
+        super(itemView);
+    }
+}
