2d03a1fae76019ed9e618604496abbc97b0d3bd4
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-22 04:07:41 +0700

    get cow and model.

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressRepository.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressRepository.java
@@ -1,15 +1,23 @@
 package com.ngadep.fatteningcattle.progresses;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.ngadep.fatteningcattle.BaseRepository;
+import com.ngadep.fatteningcattle.models.Cow;
 
 class ProgressRepository extends BaseRepository {
+    private final String COW_TREE = "cows";
+    private final String COW_PROGRESS_TREE = "cow_progresses";
+
     private static ProgressRepository INSTANCE = null;
-    private final DatabaseReference mRef;
+    private final DatabaseReference mCowRef;
+    private final DatabaseReference mCowProgressesRef;
+
 
     private ProgressRepository() {
         super();
-        mRef = getRef().child("cow-progresses");
+        mCowRef = getRef().child(COW_TREE);
+        mCowProgressesRef = getRef().child(COW_PROGRESS_TREE);
     }
 
     public static ProgressRepository getInstance() {
@@ -18,5 +26,13 @@ class ProgressRepository extends BaseRepository {
         }
 
         return INSTANCE;
+    }
+
+    public Query getCowProgressFromId(String cowId) {
+        return mCowRef.child(cowId);
+    }
+
+    public void getCowModelFromId(String cowId, ModelListener<Cow> callback) {
+        getModelFromId(mCowRef.child(cowId), callback);
     }
 }
