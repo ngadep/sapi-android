c30924f823670ec3f888267eccbbc8e6cf6214aa
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-22 04:08:10 +0700

    query database for cow progress.

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressPresenter.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressPresenter.java
@@ -1,21 +1,61 @@
 package com.ngadep.fatteningcattle.progresses;
 
-import com.ngadep.fatteningcattle.BasePresenter;
+import com.ngadep.fatteningcattle.BaseRepository;
+import com.ngadep.fatteningcattle.models.Cow;
 
-public class ProgressPresenter implements BasePresenter {
+public class ProgressPresenter implements ProgressContract.Presenter {
 
     private ProgressContract.View mView;
     private ProgressRepository mRepository;
-    private String mProgressId;
+    private String mCowId;
+    private Cow mCowModel;
+    private Long mPricePerKg;
 
-    public ProgressPresenter(ProgressContract.View view, ProgressRepository repository, String progressId) {
+    public ProgressPresenter(ProgressContract.View view, ProgressRepository repository, String cowId) {
         this.mView = view;
         this.mRepository = repository;
-        this.mProgressId = progressId;
+        this.mCowId = cowId;
     }
 
     @Override
     public void start() {
+        queryCowModel();
+        queryPricePerKg();
+        mView.getAllCowProgress(mRepository.getCowProgressFromId(mCowId));
+    }
 
+    private void queryCowModel() {
+        mRepository.getCowModelFromId(mCowId, new BaseRepository.ModelListener<Cow>() {
+            @Override
+            public void onModelChange(Cow model) {
+                mCowModel = model;
+                mView.notifyCowChange(model);
+            }
+        });
+    }
+
+    public void queryPricePerKg() {
+        mRepository.getPricePerKg(new BaseRepository.PriceListener() {
+            @Override
+            public void onPriceChange(Long price) {
+                mPricePerKg = price;
+                mView.notifyPriceChange();
+            }
+        });
+    }
+
+    @Override
+    public Long getPricePerKg() {
+        return mPricePerKg;
+    }
+
+    @Override
+    public void startProgressDetailActivity(String progressId) {
+        //TODO: start ProgressDetailActivity
+    }
+
+    @Override
+    public void cleanup() {
+        mRepository.cleanup();
     }
 }
