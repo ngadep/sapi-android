17a46fb76406802a0fc573b3e669a616e55fdbe2
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-22 04:07:15 +0700

    update ProgressContract

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressContract.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressContract.java
@@ -1,14 +1,26 @@
 package com.ngadep.fatteningcattle.progresses;
 
+import com.google.firebase.database.Query;
 import com.ngadep.fatteningcattle.BasePresenter;
 import com.ngadep.fatteningcattle.BaseView;
+import com.ngadep.fatteningcattle.models.Cow;
 
 public interface ProgressContract {
     interface Presenter extends BasePresenter {
 
+        Long getPricePerKg();
+
+        void startProgressDetailActivity(String progressId);
+
+        void cleanup();
     }
 
     interface View extends BaseView<Presenter> {
 
+        void getAllCowProgress(Query query);
+
+        void notifyPriceChange();
+
+        void notifyCowChange(Cow model);
     }
 }
