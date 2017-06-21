e96b68022e378605f769396065eaf601c3b9518b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-22 01:41:39 +0700

    update constructor for ProgressPresenter

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressPresenter.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressPresenter.java
@@ -1,21 +1,17 @@
 package com.ngadep.fatteningcattle.progresses;
 
 import com.ngadep.fatteningcattle.BasePresenter;
-import com.ngadep.fatteningcattle.cows.CowRepository;
-import com.ngadep.fatteningcattle.models.Progress;
 
 public class ProgressPresenter implements BasePresenter {
 
     private ProgressContract.View mView;
-    private CowRepository mRepository;
+    private ProgressRepository mRepository;
     private String mProgressId;
-    private Progress mProgress;
 
-    public ProgressPresenter(ProgressContract.View view, CowRepository repository, String progressId, Progress progress) {
+    public ProgressPresenter(ProgressContract.View view, ProgressRepository repository, String progressId) {
         this.mView = view;
         this.mRepository = repository;
         this.mProgressId = progressId;
-        this.mProgress = progress;
     }
 
     @Override
