4bf18642dd4798e5988c0c5deaa37529a90cbe25
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-22 04:06:12 +0700

    price dan model reference taruh di BaseRepository.

--- app/src/main/java/com/ngadep/fatteningcattle/BaseRepository.java
+++ app/src/main/java/com/ngadep/fatteningcattle/BaseRepository.java
@@ -1,13 +1,22 @@
 package com.ngadep.fatteningcattle;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class BaseRepository {
 
     protected final FirebaseDatabase mDatabase;
     protected final FirebaseAuth mAuth;
+
+    private DatabaseReference priceRef;
+    private ValueEventListener priceValueEventListener;
+
+    private DatabaseReference modelRef;
+    private ValueEventListener modelValueEventListener;
 
     protected BaseRepository() {
         mDatabase = FirebaseDatabase.getInstance();
@@ -18,11 +27,58 @@ public class BaseRepository {
         return mDatabase.getReference();
     }
 
+    public void getPricePerKg(final PriceListener callback) {
+        priceRef = getRef().child("settings").child("price");
+        priceValueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onPriceChange((Long) dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+        priceRef.addValueEventListener(priceValueEventListener);
+    }
+
+    protected void getModelFromId(DatabaseReference baseRef, final ModelListener callback) {
+        modelRef = baseRef;
+        modelValueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                callback.onModelChange(dataSnapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+        modelRef.addValueEventListener(modelValueEventListener);
+    }
+
+    public void cleanup() {
+        priceRef.removeEventListener(priceValueEventListener);
+        modelRef.removeEventListener(modelValueEventListener);
+    }
+
     public String getUid() {
         String result = "";
         if (mAuth.getCurrentUser() != null) {
             result = mAuth.getCurrentUser().getUid();
         }
         return result;
+    }
+
+    public interface PriceListener {
+        void onPriceChange(Long price);
+    }
+
+    public interface ModelListener<T> {
+        void onModelChange(T model);
     }
 }
