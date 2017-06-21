8d4367baa31552cbf7055becace0e9c258e14522
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-21 23:29:37 +0700

    bind view holder to model

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressViewHolder.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressViewHolder.java
@@ -2,10 +2,29 @@ package com.ngadep.fatteningcattle.progresses;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import com.ngadep.fatteningcattle.R;
+import com.ngadep.fatteningcattle.models.Progress;
+
+import java.util.Locale;
 
 public class ProgressViewHolder extends RecyclerView.ViewHolder {
 
+    private final TextView mCowDate;
+    private final TextView mCowWeight;
+    private final TextView mCowPrice;
+
     public ProgressViewHolder(View itemView) {
         super(itemView);
+        mCowDate = (TextView) itemView.findViewById(R.id.progress_date);
+        mCowWeight =(TextView) itemView.findViewById(R.id.progress_weight);
+        mCowPrice =(TextView) itemView.findViewById(R.id.progress_price);
+    }
+
+    public void bindToProgress(Progress model) {
+        mCowDate.setText(model.getFormatDate());
+        mCowWeight.setText(String.format(Locale.ENGLISH,"%d Kg", model.getWeight()));
+        mCowPrice.setText(String.format(Locale.ENGLISH, "Rp. %1$,.2f", model.getPrice()));
     }
 }
