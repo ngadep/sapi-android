e38a8e8bdae7c1658411048762190301a97ba936
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-23 18:19:14 +0700

    EditProgressContract

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/progresses/edit/EditProgressContract.java
@@ -0,0 +1,15 @@
+package com.ngadep.fatteningcattle.progresses.edit;
+
+
+import com.ngadep.fatteningcattle.BasePresenter;
+import com.ngadep.fatteningcattle.BaseView;
+
+public interface EditProgressContract {
+    interface Presenter extends BasePresenter {
+
+    }
+
+    interface View extends BaseView<Presenter> {
+
+    }
+}
