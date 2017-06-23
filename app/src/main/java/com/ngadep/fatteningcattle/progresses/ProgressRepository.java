ee60429fd602208ea89bc891f7149166c27d51f2
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-23 19:26:18 +0700

    getProgressFromId

--- app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressRepository.java
+++ app/src/main/java/com/ngadep/fatteningcattle/progresses/ProgressRepository.java
@@ -4,20 +4,24 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.ngadep.fatteningcattle.BaseRepository;
 import com.ngadep.fatteningcattle.models.Cow;
+import com.ngadep.fatteningcattle.models.Progress;
 
 public class ProgressRepository extends BaseRepository {
     private final String COW_TREE = "cows";
     private final String COW_PROGRESS_TREE = "cow-progresses";
+    private final String PROGRESS_TREE = "progresses";
 
     private static ProgressRepository INSTANCE = null;
     private final DatabaseReference mCowRef;
     private final DatabaseReference mCowProgressesRef;
+    private final DatabaseReference mProgressRef;
 
 
     private ProgressRepository() {
         super();
         mCowRef = getRef().child(COW_TREE);
         mCowProgressesRef = getRef().child(COW_PROGRESS_TREE);
+        mProgressRef = getRef().child(PROGRESS_TREE);
     }
 
     public static ProgressRepository getInstance() {
@@ -34,5 +38,9 @@ public class ProgressRepository extends BaseRepository {
 
     public void getCowModelFromId(String cowId, ModelListener<Cow> callback) {
         getModelFromId(mCowRef.child(cowId), Cow.class, callback);
+    }
+
+    public void getProgressFromId(String progressId, ModelListener<Progress> callback) {
+        getModelFromId(mProgressRef.child(progressId), Progress.class, callback);
     }
 }
