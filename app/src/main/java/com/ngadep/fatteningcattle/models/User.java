56ef5fdead029982b49363d1f854832fc1e4a0ce
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-25 05:20:45 +0700

    User Model not parcelable

--- app/src/main/java/com/ngadep/fatteningcattle/models/User.java
+++ app/src/main/java/com/ngadep/fatteningcattle/models/User.java
@@ -1,9 +1,6 @@
 package com.ngadep.fatteningcattle.models;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
-public class User implements Parcelable {
+public class User {
     private String userName;
     private String email;
     private boolean admin;
@@ -47,33 +44,4 @@ public class User implements Parcelable {
         this.admin = admin;
     }
 
-    protected User(Parcel in) {
-        userName = in.readString();
-        email = in.readString();
-        admin = in.readByte() != 0x00;
-    }
-
-    public static final Creator<User> CREATOR = new Creator<User>() {
-        @Override
-        public User createFromParcel(Parcel in) {
-            return new User(in);
-        }
-
-        @Override
-        public User[] newArray(int size) {
-            return new User[size];
-        }
-    };
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(userName);
-        dest.writeString(email);
-        dest.writeByte((byte) (admin ? 0x01 : 0x00));
-    }
 }
