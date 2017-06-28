4b9e093afd7ff2b17ec3afde09f9456ff83fb227
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-28 19:24:54 +0700

    RegisterPresenterTest testRegisterSuccess and testRegisterFailed

--- app/src/test/java/com/ngadep/fatteningcattle/users/register/RegisterPresenterTest.java
+++ app/src/test/java/com/ngadep/fatteningcattle/users/register/RegisterPresenterTest.java
@@ -1,5 +1,6 @@
 package com.ngadep.fatteningcattle.users.register;
 
+import com.ngadep.fatteningcattle.models.User;
 import com.ngadep.fatteningcattle.users.UserRepository;
 
 import org.junit.Before;
@@ -7,10 +8,15 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.eq;
 
 public class RegisterPresenterTest {
+
+    private static final String USER_EMAIL = "testing@gmail.com";
+    private static final String USER_PASSWORD = "password";
 
     @Mock
     RegisterContract.View mView;
@@ -25,7 +31,28 @@ public class RegisterPresenterTest {
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         mPresenter = new RegisterPresenter(mView, mRepository);
+    }
+
+    @Test
+    public void testRegisterSuccess() {
+        User user = new User(USER_EMAIL, USER_PASSWORD);
+        mPresenter.saveUser(user, USER_PASSWORD);
+
+        Mockito.verify(mRepository).registerUser(eq(user), eq(USER_PASSWORD), mRegisterListenerCaptor.capture());
+        mRegisterListenerCaptor.getValue().onRegister(true);
+        Mockito.verify(mView).registerSuccess();
+    }
+
+    @Test
+    public void testRegisterFailed() {
+        User user = new User(USER_EMAIL, USER_PASSWORD);
+        mPresenter.saveUser(user, USER_PASSWORD);
+
+        Mockito.verify(mRepository).registerUser(eq(user), eq(USER_PASSWORD), mRegisterListenerCaptor.capture());
+        mRegisterListenerCaptor.getValue().onRegister(false);
+        Mockito.verify(mView).showRegisterFailed();
     }
 
 }
