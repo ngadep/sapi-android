a743b523aa74f8801a8e0c973b40b8852e4cc1b6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-28 21:31:19 +0700

    ganti peggunaan UserView ganti dengan UserContract

---
+++ app/src/admin/java/com/ngadep/fatteningcattle/users/UserContract.java
@@ -0,0 +1,19 @@
+package com.ngadep.fatteningcattle.users;
+
+import com.google.firebase.database.Query;
+import com.ngadep.fatteningcattle.BasePresenter;
+import com.ngadep.fatteningcattle.BaseView;
+
+interface UserContract {
+    interface Presenter extends BasePresenter {
+
+        void startUserPackageActivity(String userKey);
+    }
+
+    interface View extends BaseView<Presenter> {
+        void showAllUser(Query users);
+
+        void startPackageActivity(String userId);
+
+    }
+}
