821f4ede67ae9d30d944945ce8e666d707df747f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-06-28 20:44:50 +0700

    testStartPresenter_fromSelectedUser

--- app/src/test/java/com/ngadep/fatteningcattle/packages/PackagePresenterTest.java
+++ app/src/test/java/com/ngadep/fatteningcattle/packages/PackagePresenterTest.java
@@ -1,14 +1,22 @@
 package com.ngadep.fatteningcattle.packages;
 
+import com.ngadep.fatteningcattle.BaseRepository;
+import com.ngadep.fatteningcattle.models.User;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class PackagePresenterTest {
+    private static final String USER_NAME = "Testing App";
+    private static final String USER_EMAIL = "testing@gmail.com";
 
     // random user id
     private static final java.lang.String USER_ID = "lkj234jk9j9";
@@ -19,6 +27,9 @@ public class PackagePresenterTest {
 
     @Mock
     private PackageRepository mRepository;
+
+    @Captor
+    private ArgumentCaptor<BaseRepository.ModelListener<User>> mModelListenerCaptor;
 
     @Before
     public void setUp() {
@@ -32,11 +43,9 @@ public class PackagePresenterTest {
     }
 
     @Test
-    public void testStartPresenter_newPackage() {
+    public void testStartPresenter_fromCurrentUser() {
         mPresenter = new PackagePresenter(null, mView, mRepository);
         mPresenter.start();
-
-        when(mRepository.getUid()).thenReturn(USER_ID);
 
         verify(mRepository).getUid();
 
@@ -44,6 +53,21 @@ public class PackagePresenterTest {
         verify(mView).getPackages(mRepository.getPackagesFromUserId(null));
     }
 
+    @Test
+    public void testStartPresenter_fromSelectedUser() {
+        mPresenter = new PackagePresenter(USER_ID, mView, mRepository);
+        mPresenter.start();
+
+        User user = new User(USER_NAME, USER_EMAIL);
+
+        Mockito.verify(mRepository).getUserFromId(eq(USER_ID), mModelListenerCaptor.capture());
+        mModelListenerCaptor.getValue().onModelChange(user);
+
+        mView.notifyUserChange(user);
+
+        verify(mRepository).getPackagesFromUserId(USER_ID);
+        verify(mView).getPackages(mRepository.getPackagesFromUserId(USER_ID));
+    }
 
 
 }
