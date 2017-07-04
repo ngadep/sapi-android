6efb3ecbd69002ec5793349163041ef8991b996c
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-04 22:02:46 +0700

    Log for LoginActivity

--- app/src/main/java/com/ngadep/fatteningcattle/users/login/LoginActivity.java
+++ app/src/main/java/com/ngadep/fatteningcattle/users/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.ngadep.fatteningcattle.users.login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.ngadep.fatteningcattle.R;
 import com.ngadep.fatteningcattle.users.UserRepository;
@@ -9,9 +10,13 @@ import com.ngadep.fatteningcattle.utils.ActivityUtils;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate Login Activity");
+
         setContentView(R.layout.login_act);
 
         LoginFragment loginFragment = (LoginFragment) getSupportFragmentManager()
