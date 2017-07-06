59a2baaf685f6288f9d19ae40b14cb981d4af613
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-06 20:49:24 +0700

    Log ProgressFragment

--- app/src/admin/java/com/ngadep/fatteningcattle/progresses/ProgressFragment.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/progresses/ProgressFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,24 +18,29 @@ import com.ngadep.fatteningcattle.progresses.edit.EditProgressFragment;
 public class ProgressFragment extends BaseProgressFragment {
     private static final int REQUEST_EDIT_PACKAGE = 1;
     private static final int REQUEST_EDIT_COW = 2;
+    private static final String TAG = "ProgressFragment";
 
     public static ProgressFragment newInstance() {
+        Log.i(TAG, "newInstance");
         return new ProgressFragment();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.i(TAG, "onCreatedOptionMenu");
         inflater.inflate(R.menu.menu_progress, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "onOptionsItemSelected, itemId: " + String.valueOf(item.getItemId()));
         switch (item.getItemId()) {
             case R.id.action_edit_cow:
                 mPresenter.showEditCowActivity();
@@ -45,6 +51,8 @@ public class ProgressFragment extends BaseProgressFragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "onActivityResult, requestCode: " + String.valueOf(requestCode) +
+                ", resultCode: " + String.valueOf(resultCode));
         super.onActivityResult(requestCode, resultCode, data);
         if (REQUEST_EDIT_PACKAGE == requestCode && Activity.RESULT_OK == resultCode) {
             showMessage(getString(R.string.progress_message_successfully_saved));
@@ -53,6 +61,7 @@ public class ProgressFragment extends BaseProgressFragment {
 
     @Override
     public void startEditProgressActivity(String cowId, String progressId) {
+        Log.i(TAG, "startEditProgressActivity");
         Intent intent = new Intent(getActivity(), EditProgressActivity.class);
         intent.putExtra(EditProgressFragment.ARGUMENT_EDIT_COW_ID, cowId);
         intent.putExtra(EditProgressFragment.ARGUMENT_EDIT_PROGRESS_ID, progressId);
@@ -61,6 +70,7 @@ public class ProgressFragment extends BaseProgressFragment {
 
     @Override
     public void startEditCowActivity(String cowId, String packageId) {
+        Log.i(TAG, "startEditCowActivity");
         Intent intent = new Intent(getActivity(), EditCowActivity.class);
         intent.putExtra(EditCowFragment.ARGUMENT_EDIT_COW_ID, cowId);
         intent.putExtra(EditCowFragment.ARGUMENT_EDIT_PACKAGE_ID, packageId);
