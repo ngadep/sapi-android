d06af13fa8c164181dfae63c28c12d85a5c361d8
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-06 20:52:40 +0700

    Log PackageFragmentAdmin

--- app/src/admin/java/com/ngadep/fatteningcattle/packages/PackageFragmentAdmin.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/packages/PackageFragmentAdmin.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -17,16 +18,20 @@ import com.ngadep.fatteningcattle.packages.edit.EditPackageFragment;
 
 public class PackageFragmentAdmin extends PackageFragment {
     private static final int REQUEST_ADD_PACKAGE = 1;
+    private static final String TAG = "PackageFragmentAdmin";
 
     public PackageFragmentAdmin() {
     }
 
     public static PackageFragmentAdmin newInstance() {
+        Log.i(TAG, "newInstance");
         return new PackageFragmentAdmin();
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "onActivityResult, requestCode: " + String.valueOf(requestCode) +
+                ", resultCode: " + String.valueOf(resultCode));
         super.onActivityResult(requestCode, resultCode, data);
         if (REQUEST_ADD_PACKAGE == requestCode && Activity.RESULT_OK == resultCode) {
             showMessage(getString(R.string.package_message_successfully_saved));
@@ -35,6 +40,7 @@ public class PackageFragmentAdmin extends PackageFragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
             FloatingActionButton fab =
@@ -56,11 +62,13 @@ public class PackageFragmentAdmin extends PackageFragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        Log.i(TAG, "onCreateOptionMenu");
         inflater.inflate(R.menu.menu_packages, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(TAG, "onOptionsItemSelected, itemId: " + String.valueOf(item.getItemId()));
         switch (item.getItemId()) {
             case R.id.action_edit_user:
                 /*TODO show Edit User UI*/
