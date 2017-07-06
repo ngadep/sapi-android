2d49ea5ecd6c90f1cdd1ec7001e4075342ec7f4d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-06 20:40:28 +0700

    Log MainActivity

--- app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
+++ app/src/admin/java/com/ngadep/fatteningcattle/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ngadep.fatteningcattle.main;
 
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.View;
 
 import com.ngadep.fatteningcattle.R;
@@ -12,13 +13,17 @@ import java.util.List;
 
 public class MainActivity extends BaseMainActivity {
 
+    private static final String TAG = "MainActivity";
+
     @Override
     public void setFabVisibility(FloatingActionButton fab) {
+        Log.i(TAG, "setFabVisibility");
         fab.setVisibility(View.VISIBLE);
     }
 
     @Override
     public List<MainFragment> getFragments() {
+        Log.i(TAG, "getFragments");
         List<MainFragment> result = new ArrayList<>();
         result.add(new MainFragment(R.string.heading_main_user, UserFragment.newInstance()));
         result.add(new MainFragment(R.string.heading_main_news, new NewsFragment()));
