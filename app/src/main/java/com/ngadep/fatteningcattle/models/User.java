128e620d4009c679ea745a7b5d674fb3aac0e283
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2017-07-11 22:31:49 +0700

    Lint User Model

--- app/src/main/java/com/ngadep/fatteningcattle/models/User.java
+++ app/src/main/java/com/ngadep/fatteningcattle/models/User.java
@@ -24,16 +24,8 @@ public class User {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public boolean isAdmin() {
